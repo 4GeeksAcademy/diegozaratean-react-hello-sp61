db7d327dfbf9877e9bef6373a07ccad4b77ae9d7
diego <diegozaratean@gmail.com>
2024-03-20 19:16:54 +0000

    finish class 1 react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
+// 4. Importar componente
+import Navbar from './navbar.jsx'
+// 5. Usar componente
+
 const Home = () => {
 	return (		
-		<h1 className="text-center mt-5">Hola bienvenidos a React :)!</h1>		
+		<>
+			<Navbar />
+			<h1 className="text-center mt-5">Hola bienvenidos a React :) 123!</h1>		
+			<h2>React no es tan dificil como pensaba </h2>
+		</>
 	);
 };
 
