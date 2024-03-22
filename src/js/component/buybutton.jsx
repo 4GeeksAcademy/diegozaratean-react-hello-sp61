b2033995951a1564e7e4546aa7d2e3143b7f4b4a
diego <diegozaratean@gmail.com>
2024-03-22 19:02:01 +0000

    finish class react component

---
+++ src/js/component/buybutton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Buybutton = ()=>{
+    return (
+        <button className="btn btn-primary px-5 mb-5 buybutton" type="button">
+            compra la segunda aprte
+        </button>
+    )
+}
+
+export default Buybutton
+
+
+// 4. Importar componente
+// 5. Usar componente
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 // 4. Importar componente
 import Navbar from './navbar.jsx'
+import Jumbotron from "./jumbotron.jsx";
+import Buybutton from "./buybutton.jsx";
 // 5. Usar componente
 
 const Home = () => {
 	return (		
 		<>
 			<Navbar />
+			<Jumbotron />
 			<h1 className="text-center mt-5">Hola bienvenidos a React :) 123!</h1>		
 			<h2>React no es tan dificil como pensaba </h2>
+			<Buybutton/>
 		</>
 	);
 };
